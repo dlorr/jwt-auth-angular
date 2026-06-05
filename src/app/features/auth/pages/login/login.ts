b20a3ec1caeb10431f26cc2313f50f5956e2f84b
dlorr <hyesrold@gmail.com>
2026-06-05 20:11:17 +0800

    phase 4 - login

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+import { Card } from '../../../../shared/components/ui/card/card';
+import { Button } from '../../../../shared/components/ui/button/button';
+import { ErrorMessage } from '../../../../shared/components/feedback/error-message/error-message';
+
+import { emailValidator } from '../../../../shared/validators/email.validator';
+import { passwordValidator } from '../../../../shared/validators/password.validator';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, Card, Button, ErrorMessage],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private fb = inject(FormBuilder);
+
+  submitted = false;
+
+  form = this.fb.group({
+    email: ['', [emailValidator]],
+    password: ['', [passwordValidator]],
+  });
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('LOGIN PAYLOAD:', this.form.getRawValue());
+  }
+}
