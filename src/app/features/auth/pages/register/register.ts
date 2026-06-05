d2745152c590e8abee68e07ca7a248363cd1d4fc
dlorr <hyesrold@gmail.com>
2026-06-05 20:39:36 +0800

    phase 4 - register

--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+import { Card } from '../../../../shared/components/ui/card/card';
+import { Button } from '../../../../shared/components/ui/button/button';
+import { ErrorMessage } from '../../../../shared/components/feedback/error-message/error-message';
+
+import { emailValidator } from '../../../../shared/validators/email.validator';
+import { passwordValidator } from '../../../../shared/validators/password.validator';
+import { confirmPasswordValidator } from '../../../../shared/validators/confirm-password.validator';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, Card, Button, ErrorMessage],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  private fb = inject(FormBuilder);
+
+  submitted = false;
+
+  form = this.fb.group(
+    {
+      email: ['', [emailValidator]],
+      password: ['', [passwordValidator]],
+      confirmPassword: [''],
+    },
+    {
+      validators: [confirmPasswordValidator('password', 'confirmPassword')],
+    },
+  );
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('REGISTER PAYLOAD:', this.form.getRawValue());
+  }
+}
